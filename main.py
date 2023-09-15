fa2ef168e4048e0cb89955aea9466ebb64dc995d
LCY <1149385543@qq.com>
2023-09-15 10:41:01 +0800

    修复移动端转化问题

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import pyautogui
 import time
-import datetime
 import os
 import cv2
 import random
-
-# os.system('"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"')
-
-'''
-Description: 截取当前画面
-return *
-Author: LCY
-'''
-
 
 def shotSCreen():
     image_dir = r"./screen-image"
     image_file = f"{image_dir}/screen.jpg"
     pyautogui.screenshot(image_file)
 
+def imageMatch(srctempl: str):
+    # 截取当前画面
+    shotSCreen()
 
-'''
-Description: 点击指定图片位置
-param * srctempl  
-return *
-Author: LCY
-'''
+    screenScale = 1
 
+    # 读取模板图像和屏幕截图图像
+    target = cv2.imread(srctempl)
+    temp = cv2.imread(r'.\screen-image\screen.jpg')
 
-def ImageMatch(srctempl: str):
+    tempheight, tempwidth = temp.shape[:2]
+    # 先缩放屏幕截图 INTER_LINEAR INTER_AREA
+    scaleTemp = cv2.resize(
+        temp, (int(tempwidth / screenScale), int(tempheight / screenScale)))
+
+    # 匹配图片
+    res = cv2.matchTemplate(scaleTemp, target, cv2.TM_CCOEFF_NORMED)
+    mn_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    return max_val >= 0.8
+
+def imageClick(srctempl: str):
     # 截取当前画面
     shotSCreen()
 
@@ -42,7 +43,7 @@ def ImageMatch(srctempl: str):
     # 先缩放屏幕截图 INTER_LINEAR INTER_AREA
     scaleTemp = cv2.resize(
         temp, (int(tempwidth / screenScale), int(tempheight / screenScale)))
-    
+
     # 匹配图片
     res = cv2.matchTemplate(scaleTemp, target, cv2.TM_CCOEFF_NORMED)
     mn_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
@@ -57,49 +58,33 @@ def ImageMatch(srctempl: str):
 
         # 左键点击屏幕上的这个位置
         pyautogui.click(center_x, center_y, button='left')
-
-
-'''
-Description: 搜索
-return *
-Author: LCY
-'''
-def search(q: str):
-    ImageMatch(r'.\click-img\search_input.png')
-
+    
+def autoSearch(count: int):
+    index = 0
+    while index < count:
+        imageClick(r'.\click-img\search_input.png')
+        search = random.randint(0, 999999999)
+        pyautogui.typewrite(f"https://cn.bing.com/search?q={search}")
+        time.sleep(1)
+        pyautogui.press("enter")
+        pyautogui.press("enter")
+        time.sleep(1)
+        index += 1
 
 if __name__ == "__main__":
-    search_list = ["q", "w", "e", "r"]
     # 打开浏览器
     os.system('"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"')
     time.sleep(1)
-    # 点击初始地址栏
-    ImageMatch(r'.\click-img\search_start.png')
 
     # 开始PC循环搜索
-    count = 0
-    while count < 40:
-        search = random.randint(0, 999999999)
-        pyautogui.typewrite(f"https://cn.bing.com/search?q={search}")
-        time.sleep(1)
-        pyautogui.press("enter")
-        pyautogui.press("enter")
-        time.sleep(1)
-        shotSCreen()
-        ImageMatch(r'.\click-img\search_input.png')
-        count += 1
+    autoSearch(34)
+
+    # 转换到移动端
+    pyautogui.press("f12")
+    time.sleep(1)
+    if (imageMatch(r'.\click-img\if-phone.png') == False):
+        imageClick(r'.\click-img\f12-off.png')
+    time.sleep(1)
 
     # 开始移动端循环搜索
-    pyautogui.press("f12")
-    ImageMatch(r'.\click-img\search_input.png')
-    count = 0
-    while count < 30:
-        search = random.randint(0, 999999999)
-        pyautogui.typewrite(f"https://cn.bing.com/search?q={search}")
-        time.sleep(1)
-        pyautogui.press("enter")
-        pyautogui.press("enter")
-        time.sleep(1)
-        shotSCreen()
-        ImageMatch(r'.\click-img\search_input.png')
-        count += 1
+    autoSearch(21)
